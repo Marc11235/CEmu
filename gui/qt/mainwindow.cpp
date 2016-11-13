f08e9e1a066f0ef68f0c234e37e8f265db2116a4
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-11-13 02:22:06 +0100

    [Lua] Init engine

--- gui/qt/mainwindow.cpp
+++ gui/qt/mainwindow.cpp
@@ -238,6 +238,12 @@ MainWindow::MainWindow(CEmuOpts cliOpts,QWidget *p) : QMainWindow(p), ui(new Ui:
     connect(stepOutShortcut, &QShortcut::activated, this, &MainWindow::stepOutPressed);
     connect(asmShortcut, &QShortcut::activated, this, &MainWindow::sendASMKey);
 
+    // Lua
+    connect(ui->buttonRunLuaScript, &QPushButton::clicked, this, &MainWindow::runLuaScript);
+    connect(ui->buttonLoadLuaScript, &QPushButton::clicked, this, &MainWindow::loadLuaScript);
+    connect(ui->buttonSaveLuaScript, &QPushButton::clicked, this, &MainWindow::saveLuaScript);
+    initLuaThings();
+
     // Meta Types
     qRegisterMetaType<uint32_t>("uint32_t");
     qRegisterMetaType<std::string>("std::string");
@@ -1488,4 +1494,33 @@ void MainWindow::stepOutPressed() {
     debuggerUpdateChanges();
     disconnect(stepOutShortcut, &QShortcut::activated, this, &MainWindow::stepOutPressed);
     emit setDebugStepOutMode();
+}
+
+void MainWindow::initLuaThings() {
+    static bool isInited = false;
+    if (!isInited) {
+        lua.set_panic( [](lua_State* L) {
+            const char* message = lua_tostring(L, -1);
+            if (message) {
+                lua_pop(L, 1);
+                fprintf(stderr, "[Lua Panic] %s", message);
+            }
+            return -1;
+        });
+
+        lua.open_libraries(sol::lib::base, sol::lib::package);
+
+        isInited = true;
+    }
+}
+
+void MainWindow::loadLuaScript() {
+
+}
+
+void MainWindow::saveLuaScript() {
+
+}
+
+void MainWindow::runLuaScript() {
 }
